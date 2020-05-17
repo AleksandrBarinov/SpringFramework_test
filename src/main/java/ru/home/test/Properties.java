af99b80af5c4fc6f2a822cd935fedacfb02cff96
AleksandrBarinov <nosprut@gmail.com>
2020-05-17 17:09:52 +0300

    @ConfigurationProperties

---
+++ src/main/java/ru/home/test/Properties.java
@@ -0,0 +1,19 @@
+package ru.home.test;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("application")
+public class Properties {
+
+    private boolean customProperty;
+
+    public boolean isCustomProperty() {
+        return customProperty;
+    }
+
+    public void setCustomProperty(boolean customProperty) {
+        this.customProperty = customProperty;
+    }
+}
--- src/main/java/ru/home/test/service/PersonService.java
+++ src/main/java/ru/home/test/service/PersonService.java
@@ -1,6 +1,7 @@
 package ru.home.test.service;
 
 import org.springframework.stereotype.Service;
+import ru.home.test.Properties;
 import ru.home.test.dao.PersonRepository;
 
 @Service
@@ -8,11 +9,17 @@ public class PersonService {
 
     private final PersonRepository personRepository;
 
-    public PersonService(PersonRepository personRepository) {
+    private boolean customProperty;
+
+    public PersonService(
+            PersonRepository personRepository,
+            Properties properties
+    ) {
         this.personRepository = personRepository;
+        this.customProperty = properties.isCustomProperty();
     }
 
     public String getPersonName() {
-        return personRepository.getPerson().getName();
+        return personRepository.getPerson().getName() + " " + customProperty;
     }
 }

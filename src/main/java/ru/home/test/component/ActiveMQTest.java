8172284172417616a4c23c2699b7b734f6627ea6
AleksandrBarinov <nosprut@gmail.com>
2021-02-25 01:55:06 +0300

    add ActiveMQTest

--- src/main/java/ru/home/test/component/ActiveMQTest.java
+++ src/main/java/ru/home/test/component/ActiveMQTest.java
@@ -1,10 +1,13 @@
 package ru.home.test.component;
 
+import org.apache.activemq.command.ActiveMQTextMessage;
+import org.apache.activemq.util.ByteSequence;
 import org.springframework.context.ApplicationContext;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import javax.jms.Message;
 
 @Component
 public class ActiveMQTest {
@@ -19,5 +22,17 @@ public class ActiveMQTest {
     public void testSendMessage() {
         JmsTemplate jms = context.getBean(JmsTemplate.class);
         jms.convertAndSend("foo.bar", "test message 2");
+    }
+
+    @PostConstruct
+    public void testReceiveMessage() {
+        JmsTemplate jms = context.getBean(JmsTemplate.class);
+        Message message = jms.receive("foo.bar.incoming");
+
+        assert message != null;
+        ByteSequence bytes = ((ActiveMQTextMessage) message).getContent();
+        String stringMessage = new String(bytes.data);
+
+        System.out.println(stringMessage);
     }
 }
